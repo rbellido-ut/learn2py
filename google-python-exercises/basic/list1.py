a002e18fa0c4d7779af21ff941ba428552a41ee2
Ronald Bellido <rbellido54@gmail.com>
2012-06-01 22:08:53 -0700

    Finished list1.py

--- google-python-exercises/basic/list1.py
+++ google-python-exercises/basic/list1.py
@@ -21,9 +21,11 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
-  return
-
+   count = 0
+   for i in range(len(words)):
+      if ((len(words[i]) >= 2) and (words[i][0] == words[i][-1])):
+         count += 1
+   return count
 
 # B. front_x
 # Given a list of strings, return a list with the strings
@@ -33,10 +35,17 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-  # +++your code here+++
-  return
-
-
+   # extract all the words that start with an 'x' from the list
+   # and put it in a different list
+   xlist = []
+   wlist = []
+   for elem in words:
+      if (elem.startswith('x')):
+         xlist.append(elem)
+      else:
+         wlist.append(elem)
+   #sort each separated list
+   return sorted(xlist) + sorted(wlist)
 
 # C. sort_last
 # Given a list of non-empty tuples, return a list sorted in increasing
@@ -45,9 +54,9 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-  # +++your code here+++
-  return
-
+   return sorted(tuples, key=last)
+def last(t):
+   return t[1]
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
@@ -75,7 +84,7 @@ def main():
   test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
        ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
 
-       
+
   print
   print 'sort_last'
   test(sort_last([(1, 3), (3, 2), (2, 1)]),
